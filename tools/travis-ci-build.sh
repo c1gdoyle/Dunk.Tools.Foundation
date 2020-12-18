a1dfd6a1c8ee071ed577714d225e55f30161bae1
c1gdoyle <c1gdoyle@gmail.com>
2020-12-18 18:07:44 +0000

    Add support for travis ci build

---
+++ tools/travis-ci-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Run Release build
+echo "Running build..."
+dotnet build /p:Configuration=Release ./Dunk.Tools.Foundation.sln
+
+# Run Tests, Coverlet to record result and code-coverage
+echo "Running tests..."
+dotnet test /p:Configuration=Release --no-build ./Dunk.Tools.Foundation.Test/Dunk.Tools.Foundation.Test.csproj --logger:trx /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
