127e14b2d93485ce50929ffee56c3f901048ea3b
c1gdoyle <c1gdoyle@gmail.com>
2020-12-18 22:27:03 +0000

    Add comments

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -9,7 +9,7 @@ fi
 echo "Running build..."
 dotnet build /p:Configuration=Release ./Dunk.Tools.Foundation.sln
 
-# Run Tests, Coverlet to record result and code-coverage
+# Run Tests, Coverlet to record result and OpenCover code-coverage
 echo "Running tests..."
 dotnet test /p:Configuration=Release --no-build ./Dunk.Tools.Foundation.Test/Dunk.Tools.Foundation.Test.csproj --logger:trx /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
