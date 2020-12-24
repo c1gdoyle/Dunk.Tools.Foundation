1ac5333a7666a4d2ea2fe431d2d54888ac386b4a
c1gdoyle <c1gdoyle@gmail.com>
2020-12-24 17:25:20 +0000

    Issue-17: Add NullableParsingExtension

---
+++ Dunk.Tools.Foundation.Test/Extensions/NullableParsingExtensionsTests.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Globalization;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Test.Extensions
+{
+    [TestFixture]
+    public class NullableParsingExtensionsTests
+    {
+        [Test]
+        public void ParseCharReturnsValueIfStringIsValid()
+        {
+            string s = "c";
+            char? c = s.ParseNullableChar();
+
+            Assert.IsTrue(c.HasValue);
+            Assert.AreEqual('c', c.Value);
+        }
+
+        [Test]
+        public void ParseCharReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "";
+            char? c = s.ParseNullableChar();
+
+            Assert.IsFalse(c.HasValue);
+        }
+
+        [Test]
+        public void ParseBooleanReturnsValueIfStringIsValid()
+        {
+            string s = true.ToString();
+            bool? b = s.ParseNullableBoolean();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(true, b.Value);
+        }
+
+        [Test]
+        public void ParseBooleanReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            bool? b = s.ParseNullableBoolean();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseByteReturnsValueIfStringIsValid()
+        {
+            string s = byte.MinValue.ToString();
+            byte? b = s.ParseNullableByte();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(byte.MinValue, b.Value);
+        }
+
+        [Test]
+        public void ParseByteReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            byte? b = s.ParseNullableByte();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseShortReturnsValueIfStringIsValid()
+        {
+            string s = short.MinValue.ToString();
+            short? b = s.ParseNullableInt16();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(short.MinValue, b.Value);
+        }
+
+        [Test]
+        public void ParseShortReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            short? b = s.ParseNullableInt16();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseIntReturnsValueIfStringIsValid()
+        {
+            string s = int.MinValue.ToString();
+            int? b = s.ParseNullableInt32();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(int.MinValue, b.Value);
+        }
+
+        [Test]
+        public void ParseIntReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            int? b = s.ParseNullableInt32();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseLongReturnsValueIfStringIsValid()
+        {
+            string s = long.MaxValue.ToString();
+            long? b = s.ParseNullableInt64();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(long.MaxValue, b.Value);
+        }
+
+        [Test]
+        public void ParseLongReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            long? b = s.ParseNullableInt64();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseDecimalReturnsValueIfStringIsValid()
+        {
+            string s = 300.5m.ToString(CultureInfo.InvariantCulture);
+            decimal? b = s.ParseNullableDecimal();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(300.5m, b.Value);
+        }
+
+        [Test]
+        public void ParseDecimalReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            decimal? b = s.ParseNullableDecimal();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseFloatReturnsValueIfStringIsValid()
+        {
+            string s = 300.5F.ToString(CultureInfo.InvariantCulture);
+            float? b = s.ParseNullableFloat();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(300.5F, b.Value);
+        }
+
+        [Test]
+        public void ParseFloatReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            float? b = s.ParseNullableFloat();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseDoubleReturnsValueIfStringIsValid()
+        {
+            string s = 3.1415926536.ToString(CultureInfo.InvariantCulture);
+            double? b = s.ParseNullableDouble();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(3.1415926536, b.Value);
+        }
+
+        [Test]
+        public void ParseDoubleReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            double? b = s.ParseNullableDouble();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseDateTimeReturnsValueIfStringIsValid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTime();
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(dt, b.Value);
+        }
+
+        [Test]
+        public void ParseDateTimeReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            DateTime? b = s.ParseNullableDateTime();
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseExactDateTimeReutnrsValueIfStringIsValid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-MM-dd");
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(dt, b.Value);
+        }
+
+        [Test]
+        public void ParseExactDateTimeReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-MM-dd");
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseExactDateTimeReturnsNoValueIfFormatIsInvalid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-dd-MM");
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseExactDateTimeWithSpecifiedFormatProviderReturnsValueIfStringIsValid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(dt, b.Value);
+        }
+
+        [Test]
+        public void ParseExactDateTimeWithSpecifiedFormatProviderReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseExactDateTimeWithSpecifiedFormatProviderReturnsNoValueIfFormatIsInvalid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-dd-MM", CultureInfo.InvariantCulture);
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseExactDateTimeWithSpecifiedDateStylesReturnsValueIfStringIsValid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-MM-dd", DateTimeStyles.None);
+
+            Assert.IsTrue(b.HasValue);
+            Assert.AreEqual(dt, b.Value);
+        }
+
+        [Test]
+        public void ParseExactDateTimeWithSpecifiedDateStylesReturnsNoValueIfStringIsInvalid()
+        {
+            string s = "aardvark";
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-MM-dd", DateTimeStyles.None);
+
+            Assert.IsFalse(b.HasValue);
+        }
+
+        [Test]
+        public void ParseExactDateTimeWithSpecifiedDateStylesReturnsNoValueIfFormatIsInvalid()
+        {
+            DateTime dt = new DateTime(2019, 09, 25);
+
+            string s = dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime? b = s.ParseNullableDateTimeExact("yyyy-dd-MM", DateTimeStyles.None);
+
+            Assert.IsFalse(b.HasValue);
+        }
+    }
+}
---
+++ Dunk.Tools.Foundation/Extensions/NullableParsingExtensions.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dunk.Tools.Foundation.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for parsing a string to a <see cref="Nullable{T}"/> struct.
+    /// </summary>
+    public static class NullableParsingExtensions
+    {
+        /// <summary>
+        /// A delegate that denotes the signature of the try parse handler.
+        /// </summary>
+        /// <typeparam name="T">The struct we are attempting to parse to.</typeparam>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <param name="result">
+        /// When this method returns, contains a value equivalent to the data contained in s, 
+        /// if the conversion succeeded.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if s was converted successfully; otherwise <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// It is intended that the delegate matches the TryParse method of the struct 
+        /// we are attempting to parse.
+        /// 
+        /// For example if tryinig to parse a Nullable Int32 the TryParseDelegate should 
+        /// be the TryParse method defined in <see cref="int"/>.
+        /// </remarks>
+        public delegate bool TryParseDelegate<T>(string s, out T result);
+
+        /// <summary>
+        /// Converts the string representation to a Nullable char.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="char"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static char? ParseNullableChar(this string s)
+        {
+            return ParseNullable<char>(s, char.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable boolean.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="bool"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static bool? ParseNullableBoolean(this string s)
+        {
+            return ParseNullable<bool>(s, bool.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable byte.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="byte"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static byte? ParseNullableByte(this string s)
+        {
+            return ParseNullable<byte>(s, byte.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable short.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="short"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static short? ParseNullableInt16(this string s)
+        {
+            return ParseNullable<short>(s, short.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable int.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="int"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static int? ParseNullableInt32(this string s)
+        {
+            return ParseNullable<int>(s, int.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable long.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="long"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static long? ParseNullableInt64(this string s)
+        {
+            return ParseNullable<long>(s, long.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable decimal.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="decimal"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static decimal? ParseNullableDecimal(this string s)
+        {
+            return ParseNullable<decimal>(s, decimal.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable float.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="float"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static float? ParseNullableFloat(this string s)
+        {
+            return ParseNullable<float>(s, float.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable double.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="double"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static double? ParseNullableDouble(this string s)
+        {
+            return ParseNullable<double>(s, double.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable DateTime.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="DateTime"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static DateTime? ParseNullableDateTime(this string s)
+        {
+            return ParseNullable<DateTime>(s, DateTime.TryParse);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable DateTime using a specified 
+        /// format, culture-specified format information and
+        /// <see cref="DateTimeStyles.None"/> style.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <param name="format">The required format of s.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="DateTime"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static DateTime? ParseNullableDateTimeExact(this string s, string format)
+        {
+            return ParseNullableDateTimeExact(s, format, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable DateTime using a specified 
+        /// format, culture-specified format information and
+        /// <see cref="DateTimeStyles.None"/> style.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <param name="format">The required format of s.</param>
+        /// <param name="provider">An object that supplies the culture-specific formatting information about s.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="DateTime"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static DateTime? ParseNullableDateTimeExact(this string s, string format, IFormatProvider provider)
+        {
+            return ParseNullableDateTimeExact(s, format, provider, DateTimeStyles.None);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable DateTime using specified 
+        /// format, date style and <see cref="CultureInfo.InvariantCulture"/> format 
+        /// information.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <param name="format">The required format of s.</param>
+        /// <param name="style">A bitwise combination of one or more enumeration values that indicate the permitted format of s.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="DateTime"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static DateTime? ParseNullableDateTimeExact(this string s, string format, DateTimeStyles style)
+        {
+            return ParseNullableDateTimeExact(s, format, CultureInfo.InvariantCulture, style);
+        }
+
+        /// <summary>
+        /// Converts the string representation to a Nullable DateTime using a specified
+        /// format, specified style and specified culture-specific format information.
+        /// </summary>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <param name="format">The required format of s.</param>
+        /// <param name="provider">An object that supplies the culture-specific formatting information about s.</param>
+        /// <param name="style">A bitwise combination of one or more enumeration values that indicate the permitted format of s.</param>
+        /// <returns>
+        /// A Nullable char containing the value equvialent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <see cref="DateTime"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static DateTime? ParseNullableDateTimeExact(this string s, string format, IFormatProvider provider, DateTimeStyles style)
+        {
+            return ParseNullable<DateTime>(s,
+                (string x, out DateTime y) => DateTime.TryParseExact(x, format, provider, style, out y));
+        }
+
+        /// <summary>
+        /// Converts the string representation to a <see cref="Nullable{T}"/> using a specified 
+        /// parsing delegate.
+        /// </summary>
+        /// <typeparam name="T">The value type we attempting to parse to.</typeparam>
+        /// <param name="s">A string containing the data to convert.</param>
+        /// <param name="parser">The parser to use to attempt the parsing.</param>
+        /// <returns>
+        /// A <see cref="Nullable{T}"/> instance containing the value equivalent of s.
+        /// If s was successfully parsed the <see cref="Nullable{T}.Value"/> holds a <typeparamref name="T"/> value;
+        /// otherwise contains no value.
+        /// </returns>
+        public static T? ParseNullable<T>(this string s, TryParseDelegate<T> parser)
+            where T : struct
+        {
+            T result;
+            if (parser(s, out result))
+            {
+                return (T?)result;
+            }
+            return null;
+        }
+    }
+}
