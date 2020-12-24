5daaa8f91cc3b04e1f36e7a76cc408b6a748a718
c1gdoyle <c1gdoyle@gmail.com>
2020-12-24 17:15:36 +0000

    Issue-14: Add merge-by extension for D-ary heaps

---
+++ Dunk.Tools.Foundation.Test/Extensions/MergeByExtensionsTests.cs
@@ -0,0 +1,225 @@
+﻿using System.Linq;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Test.Extensions
+{
+    [TestFixture]
+    public class MergeByExtensionsTests
+    {
+        [Test]
+        public void MergeByOrderedByCombinesSequences()
+        {
+            const int expectedCount = 14;
+
+            var a = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =1 },
+                new MergeByTestItem{ Id =8 },
+                new MergeByTestItem{ Id =13 },
+                new MergeByTestItem{ Id =19 },
+                new MergeByTestItem{ Id =40 },
+            };
+            var b = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =0 },
+                new MergeByTestItem{ Id =4 },
+                new MergeByTestItem{ Id =12 },
+                new MergeByTestItem{ Id =41 }
+            };
+            var c = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =3 },
+                new MergeByTestItem{ Id =22 },
+                new MergeByTestItem{ Id =30 },
+                new MergeByTestItem{ Id =37 },
+                new MergeByTestItem{ Id =43 },
+            };
+            var combined = new MergeByTestItem[][] { a, b, c };
+
+            var mergeOrdered = combined.MergeOrderedBy(m => m.Id);
+
+            Assert.AreEqual(expectedCount, mergeOrdered.Count());
+        }
+
+        [Test]
+        public void MergeByOrderedByLastItemIsLargest()
+        {
+            const int expectedLast = 43;
+
+            var a = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =1 },
+                new MergeByTestItem{ Id =8 },
+                new MergeByTestItem{ Id =13 },
+                new MergeByTestItem{ Id =19 },
+                new MergeByTestItem{ Id =40 },
+            };
+            var b = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =0 },
+                new MergeByTestItem{ Id =4 },
+                new MergeByTestItem{ Id =12 },
+                new MergeByTestItem{ Id =41 }
+            };
+            var c = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =3 },
+                new MergeByTestItem{ Id =22 },
+                new MergeByTestItem{ Id =30 },
+                new MergeByTestItem{ Id =37 },
+                new MergeByTestItem{ Id =43 },
+            };
+            var combined = new MergeByTestItem[][] { a, b, c };
+
+            var mergeOrdered = combined.MergeOrderedBy(m => m.Id);
+
+            Assert.AreEqual(expectedLast, mergeOrdered.Last().Id);
+        }
+
+        [Test]
+        public void MergeByOrderedByFirstItemIsSmallest()
+        {
+            const int expectedFirst = 0;
+
+            var a = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =1 },
+                new MergeByTestItem{ Id =8 },
+                new MergeByTestItem{ Id =13 },
+                new MergeByTestItem{ Id =19 },
+                new MergeByTestItem{ Id =40 },
+            };
+            var b = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =0 },
+                new MergeByTestItem{ Id =4 },
+                new MergeByTestItem{ Id =12 },
+                new MergeByTestItem{ Id =41 }
+            };
+            var c = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =3 },
+                new MergeByTestItem{ Id =22 },
+                new MergeByTestItem{ Id =30 },
+                new MergeByTestItem{ Id =37 },
+                new MergeByTestItem{ Id =43 },
+            };
+            var combined = new MergeByTestItem[][] { a, b, c };
+
+            var mergeOrdered = combined.MergeOrderedBy(m => m.Id);
+
+            Assert.AreEqual(expectedFirst, mergeOrdered.First().Id);
+        }
+
+        [Test]
+        public void MergeByOrderedByDescendingCombinesSequences()
+        {
+            const int expectedCount = 14;
+
+            var a = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =40 },
+                new MergeByTestItem{ Id =19},
+                new MergeByTestItem{ Id =13 },
+                new MergeByTestItem{ Id =8 },
+                new MergeByTestItem{ Id =1 },
+            };
+            var b = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =41 },
+                new MergeByTestItem{ Id =12 },
+                new MergeByTestItem{ Id =4 },
+                new MergeByTestItem{ Id =0 }
+            };
+            var c = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =43 },
+                new MergeByTestItem{ Id =37 },
+                new MergeByTestItem{ Id =30 },
+                new MergeByTestItem{ Id =22 },
+                new MergeByTestItem{ Id =3 },
+            };
+            var combined = new MergeByTestItem[][] { a, b, c };
+
+            var mergeOrdered = combined.MergeOrderedByDescending(m => m.Id);
+
+            Assert.AreEqual(expectedCount, mergeOrdered.Count());
+        }
+
+        [Test]
+        public void MergeByOrderedDescendindgByLastItemIsSmallest()
+        {
+            const int expectedLast = 0;
+
+            var a = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =40 },
+                new MergeByTestItem{ Id =19},
+                new MergeByTestItem{ Id =13 },
+                new MergeByTestItem{ Id =8 },
+                new MergeByTestItem{ Id =1 },
+            };
+            var b = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =41 },
+                new MergeByTestItem{ Id =12 },
+                new MergeByTestItem{ Id =4 },
+                new MergeByTestItem{ Id =0 }
+            };
+            var c = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =43 },
+                new MergeByTestItem{ Id =37 },
+                new MergeByTestItem{ Id =30 },
+                new MergeByTestItem{ Id =22 },
+                new MergeByTestItem{ Id =3 },
+            };
+            var combined = new MergeByTestItem[][] { a, b, c };
+
+            var mergeOrdered = combined.MergeOrderedByDescending(m => m.Id);
+
+            Assert.AreEqual(expectedLast, mergeOrdered.Last().Id);
+        }
+
+        [Test]
+        public void MergeByOrderedDescendingByFirstItemIsLargest()
+        {
+            const int expectedFirst = 43;
+
+            var a = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =40 },
+                new MergeByTestItem{ Id =19},
+                new MergeByTestItem{ Id =13 },
+                new MergeByTestItem{ Id =8 },
+                new MergeByTestItem{ Id =1 },
+            };
+            var b = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =41 },
+                new MergeByTestItem{ Id =12 },
+                new MergeByTestItem{ Id =4 },
+                new MergeByTestItem{ Id =0 }
+            };
+            var c = new MergeByTestItem[]
+            {
+                new MergeByTestItem{ Id =43 },
+                new MergeByTestItem{ Id =37 },
+                new MergeByTestItem{ Id =30 },
+                new MergeByTestItem{ Id =22 },
+                new MergeByTestItem{ Id =3 },
+            };
+            var combined = new MergeByTestItem[][] { a, b, c };
+
+            var mergeOrdered = combined.MergeOrderedByDescending(m => m.Id);
+
+            Assert.AreEqual(expectedFirst, mergeOrdered.First().Id);
+        }
+
+        private class MergeByTestItem
+        {
+            public int Id { get; set; }
+        }
+    }
+}
---
+++ Dunk.Tools.Foundation/Extensions/MergeByExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dunk.Tools.Foundation.Collections;
+using Dunk.Tools.Foundation.Comparers;
+
+namespace Dunk.Tools.Foundation.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for performing sorted merged on a collection of sequences.
+    /// </summary>
+    public static class MergeByExtensions
+    {
+        /// <summary>
+        /// Merges a collection of ordered sequences smallest to largest according to a specified key.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the sequene.</typeparam>
+        /// <typeparam name="TKey">The type of key returned by the <paramref name="keySelector"/></typeparam>
+        /// <param name="lists">The collection of sequences, each should be ordered smallest to largest.</param>
+        /// <param name="keySelector">A function to extract a key from an element.</param>
+        /// <returns>
+        /// A merged sequence containing all the elments of the collection of sequences, ordered smallest to largest.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="lists"/> or <paramref name="keySelector"/> was null.</exception>
+        public static IEnumerable<T> MergeOrderedBy<T, TKey>(this IEnumerable<IEnumerable<T>> lists, Func<T, TKey> keySelector)
+        {
+            return MergeOrderedBy(lists, keySelector, null);
+        }
+
+        /// <summary>
+        /// Merges a collection of ordered sequences smallest to largest according to a specified key comparer.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the sequene.</typeparam>
+        /// <typeparam name="TKey">The type of key returned by the <paramref name="keySelector"/></typeparam>
+        /// <param name="lists">The collection of sequences, each should be ordered smallest to largest.</param>
+        /// <param name="keySelector">A function to extract a key from an element.</param>
+        /// <param name="comparer">The comparer to compare the keys.</param>
+        /// <returns>
+        /// A merged sequence containing all the elments of the collection of sequences, ordered smallest to largest.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="lists"/> or <paramref name="keySelector"/> was null.</exception>
+        public static IEnumerable<T> MergeOrderedBy<T, TKey>(this IEnumerable<IEnumerable<T>> lists, Func<T, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            lists.ThrowIfNull(nameof(lists),
+                $"Unable to perform ordered merge. {nameof(lists)} parameter cannot be null.");
+            keySelector.ThrowIfNull(nameof(keySelector),
+                $"Unable to perform ordered merge. {nameof(keySelector)} parameter cannot be null.");
+
+            return MergeBy(lists, keySelector, comparer, false);
+        }
+
+        /// <summary>
+        /// Merges a collection of ordered sequences largest to smallest according to a specified key.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the sequene.</typeparam>
+        /// <typeparam name="TKey">The type of key returned by the <paramref name="keySelector"/></typeparam>
+        /// <param name="lists">The collection of sequences, each should be ordered largest to smallest.</param>
+        /// <param name="keySelector">A function to extract a key from an element.</param>
+        /// <returns>
+        /// A merged sequence containing all the elments of the collection of sequences, ordered largest to smallest.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="lists"/> or <paramref name="keySelector"/> was null.</exception>
+        public static IEnumerable<T> MergeOrderedByDescending<T, TKey>(this IEnumerable<IEnumerable<T>> lists, Func<T, TKey> keySelector)
+        {
+            return MergeOrderedByDescending(lists, keySelector, null);
+        }
+
+        /// <summary>
+        /// Merges a collection of ordered sequences largest to smallest according to a specified key comparer.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the sequene.</typeparam>
+        /// <typeparam name="TKey">The type of key returned by the <paramref name="keySelector"/></typeparam>
+        /// <param name="lists">The collection of sequences, each should be ordered largest to smallest.</param>
+        /// <param name="keySelector">A function to extract a key from an element.</param>
+        /// <param name="comparer">The comparer to compare the keys.</param>
+        /// <returns>
+        /// A merged sequence containing all the elments of the collection of sequences, ordered largest to smallest.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="lists"/> or <paramref name="keySelector"/> was null.</exception>
+        public static IEnumerable<T> MergeOrderedByDescending<T, TKey>(this IEnumerable<IEnumerable<T>> lists, Func<T, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            lists.ThrowIfNull(nameof(lists),
+                $"Unable to perform ordered descending merge. {nameof(lists)} parameter cannot be null.");
+            keySelector.ThrowIfNull(nameof(keySelector),
+                $"Unable to perform ordered descending merge. {nameof(keySelector)} parameter cannot be null.");
+
+            return MergeBy(lists, keySelector, comparer, true);
+        }
+
+        internal static IEnumerable<T> MergeBy<T, TKey>(IEnumerable<IEnumerable<T>> lists, Func<T, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+        {
+            var initialItems = lists
+                .Select(l => l.GetEnumerator())
+                .Where(i => i.MoveNext());
+
+            IComparer<T> keyComparer = comparer != null ?
+                new KeySelectorComparer<T, TKey>(keySelector, comparer) :
+                new KeySelectorComparer<T, TKey>(keySelector);
+
+            var enumerableComparer = new EnumeratorComparer<T>(keyComparer);
+
+            DHeap<IEnumerator<T>> heap;
+            if (descending)
+            {
+                heap = new MaxDHeap<IEnumerator<T>>(2, initialItems, enumerableComparer);
+            }
+            else
+            {
+                heap = new MinDHeap<IEnumerator<T>>(2, initialItems, enumerableComparer);
+            }
+
+            while (!heap.IsEmpty)
+            {
+                var i = heap.RemoveRoot();
+                yield return i.Current;
+                if (i.MoveNext())
+                {
+                    heap.Insert(i);
+                }
+            }
+        }
+    }
+}
