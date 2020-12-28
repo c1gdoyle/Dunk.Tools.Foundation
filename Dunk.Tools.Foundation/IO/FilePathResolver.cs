5e082130c79fdd13a90b893598a838b7282abdc5
c1gdoyle <c1gdoyle@gmail.com>
2020-12-28 10:51:04 +0000

    Issue-26: FilePathResolver to determine full path of file from Current-AppDomain

---
+++ Dunk.Tools.Foundation/IO/FilePathResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dunk.Tools.Foundation.IO
+{
+    /// <summary>
+    /// Responsible for resolving the path to a file in an application or windows service
+    /// base directory.
+    /// </summary>
+    public static class FilePathResolver
+    {
+        /// <summary>
+        /// Resolves the file path for a specified file.
+        /// </summary>
+        /// <param name="fileName">The name of the file.</param>
+        /// <returns>
+        /// The full file path for the file in relation to the application's 
+        /// base directory.
+        /// </returns>
+        public static string ResolveFilePath(string fileName)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            return currentDirectory + fileName;
+        }
+    }
+}
