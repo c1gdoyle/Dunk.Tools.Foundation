939328da58052f3ecf4e289a497606e540530554
c1gdoyle <c1gdoyle@gmail.com>
2020-12-28 13:31:55 +0000

    Issue-26: Add FileFacade for Append and Copy functionality

--- Dunk.Tools.Foundation.Test/IO/FileFacadeTests.cs
+++ Dunk.Tools.Foundation.Test/IO/FileFacadeTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Dunk.Tools.Foundation.IO;
 using NUnit.Framework;
 
@@ -26,7 +27,6 @@ namespace Dunk.Tools.Foundation.Test.IO
 
             FileFacade.AppendAllLines = (a, b) => { };
 
-
             Assert.AreNotEqual(method, FileFacade.AppendAllLines.Method);
         }
 
@@ -37,6 +37,180 @@ namespace Dunk.Tools.Foundation.Test.IO
 
             Assert.Throws<ArgumentNullException>(() =>
                 FileFacade.AppendAllLines = fileDelegate);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllLinesWithEncodingInitialisesWithExpectedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendAllLines", new Type[] { typeof(string), typeof(string[]), typeof(Encoding) });
+
+            Assert.AreEqual(method, FileFacade.AppendAllLinesWithEncoding.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllLinesWithEncodingSupportsSettingSpecifiedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendAllLines", new Type[] { typeof(string), typeof(string[]), typeof(Encoding) });
+
+            FileFacade.AppendAllLinesWithEncoding = (a, b, c) => { };
+
+            Assert.AreNotEqual(method, FileFacade.AppendAllLinesWithEncoding.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllLinesWithEncodingThrowsIfSpecifiedDelegateIsNull()
+        {
+            Action<string, IEnumerable<string>, Encoding> fileDelegate = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                FileFacade.AppendAllLinesWithEncoding = fileDelegate);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllTextInitialisesWithExpectedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendAllText", new Type[] { typeof(string), typeof(string) });
+
+            Assert.AreEqual(method, FileFacade.AppendAllText.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllTextSupportsSettingSpecifiedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendAllText", new Type[] { typeof(string), typeof(string) });
+
+            FileFacade.AppendAllText = (a, b) => { };
+
+            Assert.AreNotEqual(method, FileFacade.AppendAllText.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllTextThrowsIfSpecifiedDelegateIsNull()
+        {
+            Action<string, string> fileDelegate = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                FileFacade.AppendAllText = fileDelegate);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllTextWithEncodingInitialisesWithExpectedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendAllText", new Type[] { typeof(string), typeof(string), typeof(Encoding) });
+
+            Assert.AreEqual(method, FileFacade.AppendAllTextWithEncoding.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllTextWithEncodingSupportsSettingSpecifiedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendAllText", new Type[] { typeof(string), typeof(string), typeof(Encoding) });
+
+            FileFacade.AppendAllTextWithEncoding = (a, b, c) => { };
+
+            Assert.AreNotEqual(method, FileFacade.AppendAllTextWithEncoding.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendAllTextWithEncodingThrowsIfSpecifiedDelegateIsNull()
+        {
+            Action<string, string, Encoding> fileDelegate = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                FileFacade.AppendAllTextWithEncoding = fileDelegate);
+        }
+
+        [Test]
+        public void FileFacadeAppendTextInitialisesWithExpectedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendText", new Type[] { typeof(string) });
+
+            Assert.AreEqual(method, FileFacade.AppendText.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendTextSupportsSettingSpecifiedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("AppendText", new Type[] { typeof(string) });
+
+            FileFacade.AppendText = a => null;
+
+            Assert.AreNotEqual(method, FileFacade.AppendText.Method);
+        }
+
+        [Test]
+        public void FileFacadeAppendTextThrowsIfSpecifiedDelegateIsNull()
+        {
+            Func<string, StreamWriter> fileDelegate = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                FileFacade.AppendText = fileDelegate);
+        }
+
+        [Test]
+        public void FileFacadeCopyInitialisesWithExpectedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("Copy", new Type[] { typeof(string), typeof(string) });
+
+            Assert.AreEqual(method, FileFacade.Copy.Method);
+        }
+
+        [Test]
+        public void FileFacadeCopySupportsSettingSpecifiedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("Copy", new Type[] { typeof(string), typeof(string) });
+
+            FileFacade.Copy = (a, b) => { };
+
+            Assert.AreNotEqual(method, FileFacade.Copy.Method);
+        }
+
+        [Test]
+        public void FileFacadeCopyThrowsIfSpecifiedDelegateIsNull()
+        {
+            Action<string, string> fileDelegate = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                FileFacade.Copy = fileDelegate);
+        }
+
+        [Test]
+        public void FileFacadeCopyAndOverwriteInitialisesWithExpectedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("Copy", new Type[] { typeof(string), typeof(string), typeof(bool) });
+
+            Assert.AreEqual(method, FileFacade.CopyAndOverwrite.Method);
+        }
+
+        [Test]
+        public void FileFacadeCopyAndOverwriteSupportsSettingSpecifiedDelegate()
+        {
+            var method = typeof(File)
+                .GetMethod("Copy", new Type[] { typeof(string), typeof(string), typeof(bool) });
+
+            FileFacade.CopyAndOverwrite = (a, b, c) => { };
+
+            Assert.AreNotEqual(method, FileFacade.CopyAndOverwrite.Method);
+        }
+
+        [Test]
+        public void FileFacadeCopyAndOverwriteThrowsIfSpecifiedDelegateIsNull()
+        {
+            Action<string, string, bool> fileDelegate = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                FileFacade.CopyAndOverwrite = fileDelegate);
         }
 
         [SetUp]
--- Dunk.Tools.Foundation/IO/FileFacade.cs
+++ Dunk.Tools.Foundation/IO/FileFacade.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace Dunk.Tools.Foundation.IO
 {
@@ -17,9 +18,15 @@ namespace Dunk.Tools.Foundation.IO
     public static class FileFacade
     {
         private static Action<string, IEnumerable<string>> _appendAllLines = File.AppendAllLines;
+        private static Action<string, IEnumerable<string>, Encoding> _appendAllLinesWithEncoding = File.AppendAllLines;
+        private static Action<string, string> _appendAllText = File.AppendAllText;
+        private static Action<string, string, Encoding> _appendAllTextWithEncoding = File.AppendAllText;
+        private static Func<string, StreamWriter> _appendText = File.AppendText;
+        private static Action<string, string> _copy = File.Copy;
+        private static Action<string, string, bool> _copyAndOverwrite = File.Copy;
 
         /// <summary>
-        /// Gets or sets delegate over the <see cref="File.AppendAllLines(string, IEnumerable{string})"/> method.<br/>
+        /// Gets or sets a delegate over the <see cref="File.AppendAllLines(string, IEnumerable{string})"/> method.<br/>
         /// 
         /// Appends lines to a file, and then closes the file. If the specified file does 
         /// not exist, this method creates a file, writes the specified lines to the file, 
@@ -27,9 +34,8 @@ namespace Dunk.Tools.Foundation.IO
         /// 
         /// Parameters:<br/>
         ///  path:
-        ///    The file to append the lines to. The file is created if it doesn't already exist.<br/>
-        ///
-        ///   contents:
+        ///    The file to append the lines to. The file is created if it doesn't already exist.<br/>        ///
+        ///  contents:
         ///     The lines to append to the file.<br/>
         ///</summary>
         /// <exception cref="ArgumentNullException">value delegate was null.</exception>
@@ -38,12 +44,180 @@ namespace Dunk.Tools.Foundation.IO
             get { return _appendAllLines; }
             set
             {
-                if(value == null)
+                if (value == null)
                 {
                     throw new ArgumentNullException(nameof(value),
-                        $"Unable to set AppendAllLines delegate. {nameof(value)} cannot be null");
+                        $"Unable to set {nameof(AppendAllLines)} delegate. value cannot be null");
                 }
                 _appendAllLines = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a delegate over the <see cref="File.AppendAllLines(string, IEnumerable{string}, Encoding)"/> method.<br/>
+        /// 
+        /// Appends lines to a file by using a specified encoding, and then closes the file. 
+        /// If the specified file does not exist, this method creates a file, writes the 
+        /// specified lines to the file, and then closes the file.<br/>
+        /// 
+        /// Parameters:<br/>
+        ///  path:
+        ///    The file to append the lines to. The file is created if it doesn't already exist.<br/>
+        ///  contents:
+        ///     The lines to append to the file.<br/>
+        ///  encoding:
+        ///      The character encoding to use.<br/>
+        ///</summary>
+        /// <exception cref="ArgumentNullException">value delegate was null.</exception>
+        public static Action<string, IEnumerable<string>, Encoding> AppendAllLinesWithEncoding
+        {
+            get { return _appendAllLinesWithEncoding; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value),
+                        $"Unable to set {nameof(AppendAllLinesWithEncoding)} delegate. value cannot be null");
+                }
+                _appendAllLinesWithEncoding = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a delegate over the <see cref="File.AppendAllText(string, string)"/> method.<br/>
+        /// 
+        /// Opens a file, appends the specified string to the file, and then closes the file.
+        /// If the file does not exist, this method creates a file, writes the specified 
+        /// string to the file, then closes the file.<br/>#=
+        /// 
+        /// Parameters:<br/>
+        ///  path:
+        ///     The file to append the specified string to.<br/>
+        ///  contents:
+        ///     The string to append to the file.<br/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException">value delegate was null.</exception>
+        public static Action<string, string> AppendAllText
+        {
+            get { return _appendAllText; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value),
+                        $"Unable to set {nameof(AppendAllText)} delegate. value cannot be null");
+                }
+                _appendAllText = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a delegate over the <see cref="File.AppendAllText(string, string, Encoding)"/> method.<br/>
+        /// 
+        ///  Appends the specified string to the file, creating the file if it does not already 
+        ///  exist.<br/>
+        ///  
+        /// Parameters:<br/>
+        ///  path:
+        ///     The file to append the specified string to.<br/>
+        ///  contents:
+        ///     The string to append to the file.<br/>
+        ///  encoding:
+        ///     The character encoding to use.<br/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException">value delegate was null.</exception>
+        public static Action<string, string, Encoding> AppendAllTextWithEncoding
+        {
+            get { return _appendAllTextWithEncoding; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value),
+                        $"Unable to set {nameof(AppendAllTextWithEncoding)} delegate. value cannot be null");
+                }
+                _appendAllTextWithEncoding = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a delegate over the <see cref="File.AppendText(string)"/> method.<br/>
+        /// 
+        /// Creates a System.IO.StreamWriter that appends UTF-8 encoded text to an existing 
+        /// file, or to a new file if the specified file does not exist.<br/>
+        /// 
+        /// Parameters:<br/>
+        ///   path:
+        ///     The path to the file to append to.<br/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException">value delegate was null.</exception>
+        public static Func<string, StreamWriter> AppendText
+        {
+            get { return _appendText; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value),
+                        $"Unable to set {nameof(AppendText)} delegate. value cannot be null");
+                }
+                _appendText = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a delegate over the <see cref="File.Copy(string, string)"/> method.<br/>
+        ///
+        /// Copies an existing file to a new file. Overwriting a file of the same name is
+        /// not allowed.<br/>
+        /// 
+        /// Parameters:<br/>
+        ///  sourceFileName:
+        ///     The file to copy.<br/>
+        ///  destFileName:
+        ///     The name of the destination file. This cannot be a directory or an existing file.<br/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException">value delegate was null.</exception>
+        public static Action<string, string> Copy
+        {
+            get { return _copy; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value),
+                        $"Unable to set {nameof(Copy)} delegate. value cannot be null");
+                }
+                _copy = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a delegate over the <see cref="File.Copy(string, string)"/> method.<br/>
+        ///
+        /// Copies an existing file to a new file. Overwriting a file of the same name is
+        /// not allowed.<br/>
+        /// 
+        /// Parameters:<br/>
+        ///  sourceFileName:
+        ///     The file to copy.<br/>
+        ///  destFileName:
+        ///     The name of the destination file. This cannot be a directory or an existing file.<br/>
+        ///  overwrite:
+        ///     true if the destination file can be overwritten; otherwise, false.<br/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException">value delegate was null.</exception>
+        public static Action<string, string, bool> CopyAndOverwrite
+        {
+            get { return _copyAndOverwrite; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value),
+                        $"Unable to set {nameof(CopyAndOverwrite)} delegate. value cannot be null");
+                }
+                _copyAndOverwrite = value;
             }
         }
 
@@ -54,6 +228,12 @@ namespace Dunk.Tools.Foundation.IO
         public static void ResetDefaults()
         {
             AppendAllLines = File.AppendAllLines;
+            AppendAllLinesWithEncoding = File.AppendAllLines;
+            AppendAllText = File.AppendAllText;
+            AppendAllTextWithEncoding = File.AppendAllText;
+            AppendText = File.AppendText;
+            Copy = File.Copy;
+            CopyAndOverwrite = File.Copy;
         }
     }
 }
