5ef6a6ede92452be98e2af19cd260261c87bee0f
c1gdoyle <c1gdoyle@gmail.com>
2021-03-14 12:12:49 +0000

    Issue-57: Initial check-in for StandardDeviation calculations. TODO support nullable value types

---
+++ Dunk.Tools.Foundation.Test/Extensions/StandardDeviationExtensionTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Test.Extensions
+{
+    [TestFixture]
+    public class StandardDeviationExtensionTests
+    {
+        [Test]
+        public void StandardDeviationCalculatesForSequenceOfDecimals()
+        {
+            const double expected = 7.5969918858904748d;
+
+            var sequence = new decimal[] { 1m, 3m, 24m, 17m, 12m, 6m, 14m };
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationReturnsZeroForEmptySequenceOfDecimals()
+        {
+            const double expected = 0;
+
+            var sequence = Array.Empty<decimal>();
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationThrowsForNullSequenceOfDecimals()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as IEnumerable<decimal>).StandardDeviation());
+        }
+
+        [Test]
+        public void StandardDeviationCalculatesForSequenceOfFloats()
+        {
+            const double expected = 7.5969921707776651d;
+
+            var sequence = new float[] { 1f, 3f, 24f, 17f, 12f, 6f, 14f };
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationReturnsZeroForEmptySequenceOfFloats()
+        {
+            const double expected = 0;
+
+            var sequence = Array.Empty<float>();
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationThrowsForNullSequenceOfFloats()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as IEnumerable<float>).StandardDeviation());
+        }
+
+        [Test]
+        public void StandardDeviationCalculatesForSequenceOfInts()
+        {
+            const double expected = 7.5969918858904748d;
+
+            var sequence = new int [] { 1, 3, 24, 17, 12, 6, 14 };
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationReturnsZeroForEmptySequenceOfInts()
+        {
+            const double expected = 0;
+
+            var sequence = Array.Empty<int>();
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationThrowsForNullSequenceOfInts()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as IEnumerable<int>).StandardDeviation());
+        }
+
+        [Test]
+        public void StandardDeviationCalculatesForSequenceOfLongs()
+        {
+            const double expected = 7.5969918858904748d;
+
+            var sequence = new long[] { 1, 3, 24, 17, 12, 6, 14 };
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationReturnsZeroForEmptySequenceOfLongs()
+        {
+            const double expected = 0;
+
+            var sequence = Array.Empty<long>();
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationThrowsForNullSequenceOfLongs()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as IEnumerable<long>).StandardDeviation());
+        }
+
+        [Test]
+        public void StandardDeviationCalculatesForSequenceOfDoubles()
+        {
+            const double expected = 7.5969918858904748d;
+
+            var sequence = new double[] { 1d, 3d, 24d, 17d, 12d, 6d, 14d };
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationReturnsZeroForEmptySequenceOfDoubles()
+        {
+            const double expected = 0;
+
+            var sequence = Array.Empty<double>();
+
+            double result = sequence.StandardDeviation();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void StandardDeviationThrowsForNullSequenceOfDoubles()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as IEnumerable<double>).StandardDeviation());
+        }
+    }
+}
--- Dunk.Tools.Foundation/Extensions/EnumerableExtensions.cs
+++ Dunk.Tools.Foundation/Extensions/EnumerableExtensions.cs
@@ -24,5 +24,176 @@ namespace Dunk.Tools.Foundation.Extensions
 
             return source.OrderBy(x => r.Next());
         }
+
+        /// <summary>
+        /// Computes standard-deviation for a sequence of <see cref="decimal"/>s.
+        /// </summary>
+        /// <param name="source">The sequence.</param>
+        /// <returns>
+        /// The standard-deviation, or <c>0</c> if the sequence is empty.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> cannot be null.</exception>
+        public static double StandardDeviation(this IEnumerable<decimal> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source),
+                    $"Unable to calculate Standard-Deviation, {nameof(source)} parameter cannot be null");
+            }
+
+            decimal mean = 0;
+            decimal sum = 0;
+
+            long n = 0;
+            foreach (decimal value in source)
+            {
+                n++;
+                decimal delta = value - mean;
+                mean += delta / n;
+                sum += delta * (value - mean);
+            }
+            if (n > 1)
+            {
+                return Math.Sqrt((double)sum / n);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Computes standard-deviation for a sequence of <see cref="float"/>s.
+        /// </summary>
+        /// <param name="source">The sequence.</param>
+        /// <returns>
+        /// The standard-deviation, or <c>0</c> if the sequence is empty.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> cannot be null.</exception>
+        public static double StandardDeviation(this IEnumerable<float> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source),
+                    $"Unable to calculate Standard-Deviation, {nameof(source)} parameter cannot be null");
+            }
+
+            float mean = 0;
+            decimal sum = 0;
+
+            long n = 0;
+            foreach (float value in source)
+            {
+                n++;
+                float delta = value - mean;
+                mean += delta / n;
+                sum += (decimal)(delta * (value - mean));
+            }
+            if (n > 1)
+            {
+                return Math.Sqrt((double)sum / n);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Computes standard-deviation for a sequence of <see cref="int"/>s.
+        /// </summary>
+        /// <param name="source">The sequence.</param>
+        /// <returns>
+        /// The standard-deviation, or <c>0</c> if the sequence is empty.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> cannot be null.</exception>
+        public static double StandardDeviation(this IEnumerable<int> source)
+        {
+            if(source == null)
+            {
+                throw new ArgumentNullException(nameof(source),
+                    $"Unable to calculate Standard-Deviation, {nameof(source)} parameter cannot be null");
+            }
+
+            double mean = 0;
+            double sum = 0;
+
+            long n = 0;
+            foreach(int value in source)
+            {
+                n++;
+                double delta = value - mean;
+                mean += delta / n;
+                sum += delta * (value - mean);
+            }
+            if(n > 1)
+            {
+                return Math.Sqrt(sum / n);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Computes standard-deviation for a sequence of <see cref="long"/>s.
+        /// </summary>
+        /// <param name="source">The sequence.</param>
+        /// <returns>
+        /// The standard-deviation, or <c>0</c> if the sequence is empty.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> cannot be null.</exception>
+        public static double StandardDeviation(this IEnumerable<long> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source),
+                    $"Unable to calculate Standard-Deviation, {nameof(source)} parameter cannot be null");
+            }
+
+            double mean = 0;
+            double sum = 0;
+
+            long n = 0;
+            foreach (long value in source)
+            {
+                n++;
+                double delta = value - mean;
+                mean += delta / n;
+                sum += delta * (value - mean);
+            }
+            if (n > 1)
+            {
+                return Math.Sqrt(sum / n);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Computes standard-deviation for a sequence of <see cref="double"/>s.
+        /// </summary>
+        /// <param name="source">The sequence.</param>
+        /// <returns>
+        /// The standard-deviation, or <c>0</c> if the sequence is empty.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> cannot be null.</exception>
+        public static double StandardDeviation(this IEnumerable<double> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source),
+                    $"Unable to calculate Standard-Deviation, {nameof(source)} parameter cannot be null");
+            }
+
+            double mean = 0;
+            double sum = 0;
+
+            long n = 0;
+            foreach (double value in source)
+            {
+                n++;
+                double delta = value - mean;
+                mean += delta / n;
+                sum += delta * (value - mean);
+            }
+            if (n > 1)
+            {
+                return Math.Sqrt(sum / n);
+            }
+            return 0;
+        }
+
     }
 }
