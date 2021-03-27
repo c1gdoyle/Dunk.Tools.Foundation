b5cc0e277f834eebdb6fdf28ee1796803906aa35
c1gdoyle <c1gdoyle@gmail.com>
2021-03-27 13:18:07 +0000

    Issue-71: Queryable and Collection extensions

---
+++ Dunk.Tools.Foundation.Test/Extensions/CollectionExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Test.Extensions
+{
+    [TestFixture]
+    public class CollectionExtensionsTests
+    {
+        [Test]
+        public void AddRangeItemsToCollection()
+        {
+            ICollection<int> sequence = new HashSet<int> { 1, 2, 3 };
+            int[] itemsToAdd = { 4, 5, 6 };
+
+            sequence.AddRange(itemsToAdd);
+
+            Assert.AreEqual(6, sequence.Count);
+        }
+
+        [Test]
+        public void AddRangeAddsItemsToList()
+        {
+            ICollection<int> sequence = new List<int> { 1, 2, 3 };
+            int[] itemsToAdd = { 4, 5, 6 };
+
+            sequence.AddRange(itemsToAdd);
+
+            Assert.AreEqual(6, sequence.Count);
+        }
+
+        [Test]
+        public void AddRangeThrowsIfCollectionToAddIsNull()
+        {
+            ICollection<int> sequence = new List<int> { 1, 2, 3 };
+            Assert.Throws<ArgumentNullException>(() => sequence.AddRange(null as IEnumerable<int>));
+        }
+    }
+}
---
+++ Dunk.Tools.Foundation.Test/Extensions/QueryableExtensionsTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Test.Extensions
+{
+    [TestFixture]
+    public class QueryableExtensionsTests
+    {
+        [Test]
+        public void QueryableBetweenReturnsCollectionWithLowerLimit()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            var between = list.AsQueryable().Between(i => i, 5, 15);
+
+            Assert.AreEqual(5, between.Min());
+        }
+
+        [Test]
+        public void QueryableBetweenReturnsCollectionWithUpperLimit()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            var between = list.AsQueryable().Between(i => i, 5, 15);
+
+            Assert.AreEqual(15, between.Max());
+        }
+
+        [Test]
+        public void QueryableOrderBySortByDescendingUsingSpecifiedPropertyName()
+        {
+            var list = new List<TestItem>
+            {
+                new TestItem {Id=1, Name="Item_1" },
+                new TestItem {Id=2, Name="Item_2" },
+                new TestItem {Id=3, Name="Item_3" },
+                new TestItem {Id=4, Name="Item_4" },
+                new TestItem {Id=5, Name="Item_5" },
+                new TestItem {Id=6, Name="Item_6" },
+            }
+            .Randomize()
+            .ToList();
+
+            var orderedCollection = list.AsQueryable()
+                .Orderby(nameof(TestItem.Id), true);
+
+            var firstItem = orderedCollection.First();
+
+            Assert.AreEqual(6, firstItem.Id);
+        }
+
+        [Test]
+        public void QueryableOrderBySortByAscendingUsingSpecifiedPropertyName()
+        {
+            var list = new List<TestItem>
+            {
+                new TestItem {Id=1, Name="Item_1" },
+                new TestItem {Id=2, Name="Item_2" },
+                new TestItem {Id=3, Name="Item_3" },
+                new TestItem {Id=4, Name="Item_4" },
+                new TestItem {Id=5, Name="Item_5" },
+                new TestItem {Id=6, Name="Item_6" },
+            }
+            .Randomize()
+            .ToList();
+
+            var orderedCollection = list.AsQueryable()
+                .Orderby(nameof(TestItem.Id), false);
+
+            var firstItem = orderedCollection.First();
+
+            Assert.AreEqual(1, firstItem.Id);
+        }
+
+        private class TestItem
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
