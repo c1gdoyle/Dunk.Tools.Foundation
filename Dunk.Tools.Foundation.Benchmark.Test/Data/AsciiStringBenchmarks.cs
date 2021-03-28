1d9fb6b3f8fb1d224ec947db292e18e60757dcc1
c1gdoyle <c1gdoyle@gmail.com>
2021-03-28 12:29:46 +0100

    Issue-80: Add Benchmarks for Ascii-String operations

---
+++ Dunk.Tools.Foundation.Benchmark.Test/Data/AsciiStringBenchmarks.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using Dunk.Tools.Foundation.Data;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Benchmark.Test.Data
+{
+    [MemoryDiagnoser]
+    [MedianColumn]
+    [MaxColumn]
+    public class AsciiStringBenchmarks
+    {
+        private string _input;
+
+        [Params(1000, 10000, 100000, 250000)]
+        public int Number { get; set; }
+
+        [Benchmark]
+        public void AsciiStringCreation()
+        {
+            var asciiString = new AsciiString(_input);
+
+            Assert.AreEqual(Number, asciiString.Length);
+        }
+
+        [Test]
+        public void AsciiStringSubString()
+        {
+            var asciiString = new AsciiString(_input);
+            var newAsciiString = asciiString.SubString(0, Number);
+
+            Assert.AreEqual(Number, newAsciiString.Length);
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _input = StringExtensions.GenerateRandomAsciiString(Number);
+        }
+    }
+}
