84e1e55114b33dff5c753bbe7277cec9fa5a588a
c1gdoyle <c1gdoyle@gmail.com>
2021-05-15 15:11:39 +0100

    Issue-103: Add benchmarks for Predicate extensions

--- Dunk.Tools.Foundation.Benchmark.Test/Extensions/ComputeLevenshteinDistanceExtensionsBenchmarks.cs
+++ Dunk.Tools.Foundation.Benchmark.Test/Extensions/ComputeLevenshteinDistanceExtensionsBenchmarks.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using BenchmarkDotNet.Attributes;
 using Dunk.Tools.Foundation.Extensions;
 using NUnit.Framework;
---
+++ Dunk.Tools.Foundation.Benchmark.Test/Extensions/PredicateExtensionsBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using BenchmarkDotNet.Attributes;
+using Dunk.Tools.Foundation.Extensions;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Benchmark.Test.Extensions
+{
+    [MemoryDiagnoser]
+    [MedianColumn]
+    [MaxColumn]
+    public class PredicateExtensionsBenchmarks
+    {
+        private Expression<Func<int, bool>>[] _expressions;
+
+        [Params(100, 200, 500, 1000, 10000)]
+        public int Number { get; set; }
+
+        [Benchmark]
+        public void ComposeAndAlso()
+        {
+            var result = _expressions.And();
+            Assert.IsNotNull(result);
+        }
+
+        [Benchmark]
+        public void ComposeOrElse()
+        {
+            var result = _expressions.Or();
+            Assert.IsNotNull(result);
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var expressions = new Expression<Func<int, bool>> []
+            {
+                i => i > 0,
+                i => i < 5,
+                i => i > -1,
+                i => i < 6
+            };
+            _expressions = new Expression<Func<int, bool>>[Number];
+
+            for(int i = 0; i < _expressions.Length; i++)
+            {
+                _expressions[i] = expressions[i % 4];
+            }
+        }
+    }
+}
