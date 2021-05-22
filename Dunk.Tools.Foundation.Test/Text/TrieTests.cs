d77401c05346defde73538edb3e63276c1dd784f
c1gdoyle <c1gdoyle@gmail.com>
2021-05-22 18:59:59 +0100

    Issue-28: Add Trie functionality for Add and Remove

---
+++ Dunk.Tools.Foundation.Test/Text/TrieTests.cs
@@ -0,0 +1,670 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dunk.Tools.Foundation.Extensions;
+using Dunk.Tools.Foundation.Text;
+using NUnit.Framework;
+
+namespace Dunk.Tools.Foundation.Test.Text
+{
+    [TestFixture]
+    public class TrieTests
+    {
+        [Test]
+        public void TrieInitialises()
+        {
+            var trie = new Trie();
+            Assert.IsNotNull(trie);
+        }
+
+        [Test]
+        public void TrieInitialisesWithDefaultRootNode()
+        {
+            const int expected = 0;
+
+            var trie = new Trie();
+
+            Assert.AreEqual(expected, trie.Count);
+        }
+
+        [Test]
+        public void TrieInitialisesWithZeroCount()
+        {
+            var trie = new Trie();
+
+            Assert.AreEqual(TrieNode.DefaultRootTrieNode, trie.RootNode);
+        }
+
+        [Test]
+        public void TrieAddWordThrowsIfWordIsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.AddWord(null));
+        }
+
+        [Test]
+        public void TrieAddWordsThrowsIfWordsCollectionIsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.AddWords(null));
+        }
+
+        [Test]
+        public void TrieAddWordsThrowsIfWordsCollectionContainsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.AddWords(new string[] { null }));
+        }
+
+        [Test]
+        public void TrieAddsWords()
+        {
+            const int expected = 5;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.AreEqual(expected, trie.Count);
+        }
+
+        [Test]
+        public void TrieAddWordsUpdatesCountIfWordsAreAddedInDifferrentOrder()
+        {
+            const int expected = 5;
+
+            var words = new List<string> { "ARMED", "ARM", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.AreEqual(expected, trie.Count);
+        }
+
+        [Test]
+        public void TrieAddWordsUpdatesCountIfWordsAreAddedInRandomOrder()
+        {
+            const int expected = 5;
+
+            var words = new List<string> { "ARMED", "ARM", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words.Randomize());
+
+            Assert.AreEqual(expected, trie.Count);
+        }
+
+        [Test]
+        public void TrieAddWordsUpdatesCountOnceIfWordsAreAddedMultipleTimes()
+        {
+            const int expected = 1;
+
+            var words = new List<string> { "ARMED", "ARMED", "ARMED", "ARMED", "ARMED" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.AreEqual(expected, trie.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsReturnsAllWordsInTrie()
+        {
+            const int expected = 5;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var result = trie.GetWords().ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsReturnsAllWordsEnumerableSupportsTake()
+        {
+            const int expected = 2;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var result = trie.GetWords().Take(2).ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsRetunsAllWordsEnumerableSupportsSkip()
+        {
+            const int expected = 3;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var result = trie.GetWords().Skip(2).ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetsReturnsEmptyIfTrieIsEmpty()
+        {
+            const int expected = 0;
+
+            var trie = new Trie();
+
+            var result = trie.GetWords().ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsByPrefixThrowsIfPrefixIsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.GetWords(null).ToList());
+        }
+
+        [Test]
+        public void TrieGetWordsByPrefixReturnsAllWordsForPrefix()
+        {
+            const int expected = 3;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var result = trie.GetWords("AR").ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsByPrefixEnumerableSupportsTake()
+        {
+            const int expected = 2;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var result = trie.GetWords("AR").Take(2).ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsByPrefixEnumerableSupportsSkip()
+        {
+            const int expected = 1;
+
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var result = trie.GetWords("AR").Skip(2).ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetWordsByPrefixReturnsEmptyIfPrefixIsNotInTrie()
+        {
+            const int expected = 0;
+
+            var trie = new Trie();
+
+            var result = trie.GetWords("AR").ToList();
+
+            Assert.AreEqual(expected, result.Count);
+        }
+
+        [Test]
+        public void TrieGetTrieNodeReturnsNodeIfPrefixIsPresent()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var node = trie.GetTrieNode("AR");
+
+            Assert.IsNotNull(node);
+        }
+
+        [Test]
+        public void TrieGetTrieNodeReturnsNullIfPrefixIsNotPresent()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            var node = trie.GetTrieNode("FOO");
+
+            Assert.IsNull(node);
+        }
+
+        [Test]
+        public void TrieGetTrieNodeThrowsIfPrefixIsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.GetTrieNode(null));
+        }
+
+        [Test]
+        public void TrieHasPrefixThrowsIfInputIsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.HasPrefix(null));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsTrueIfTrieContainsPrefix()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsTrue(trie.HasPrefix("AR"));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsFalseIfTrieDoesNotContainPrefix()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasPrefix("BEE"));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsFalseForEmptyString()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasPrefix(string.Empty));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsFalseForDefaultChar()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasPrefix(new string(new[] { default(char) })));
+        }
+
+        [Test]
+        public void TrieHasWordThrowsIfInputIsNull()
+        {
+            var trie = new Trie();
+
+            Assert.Throws<ArgumentNullException>(() => trie.HasWord(null));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsTrueIfTrieContainsWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsTrue(trie.HasWord("ARM"));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsFalseIfTrieDoesNotContainWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasWord("BEE"));
+        }
+
+        [Test]
+        public void TrieHasPrefixReturnsFalseIfInputIsNotWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasWord("AR"));
+        }
+
+        [Test]
+        public void TrieHasWordReturnsFalseForEmptyString()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasWord(string.Empty));
+        }
+
+        [Test]
+        public void TrieHasWordReturnsFalseForDefaultChar()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.HasWord(new string(new[] { default(char) })));
+        }
+
+        [Test]
+        public void TrieClearRemovesAllChildNodes()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.Clear();
+
+            Assert.AreEqual(0, trie.Count);
+        }
+
+        [Test]
+        public void TrieClearDoesNotThrowIfNoChildNodes()
+        {
+            var trie = new Trie();
+
+            Assert.DoesNotThrow(() => trie.Clear());
+            Assert.AreEqual(0, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixReturnsFalseIfTrieDoesNotContainPrefix()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemovePrefix("FOO"));
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixReturnsTrueIfTrieDoesContainPrefix()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsTrue(trie.RemovePrefix("AR"));
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixRemovesExpectedStrings()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.RemovePrefix("AR");
+
+            Assert.IsFalse(trie.HasWord("ARM"));
+            Assert.IsFalse(trie.HasWord("ARMED"));
+            Assert.IsFalse(trie.HasWord("ARC"));
+            Assert.AreEqual(2, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixThrowsIfSpecifiedWordIsNull()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.Throws<ArgumentNullException>(() => trie.RemovePrefix(null));
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixReturnsFalseIfSpecifiedPrefixIsEmpty()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemovePrefix(string.Empty));
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixDoesNotRemoveAnyNodesIfSpecifiedPrefixIsEmpty()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.RemovePrefix(string.Empty);
+
+            words.ForEach(s =>
+                Assert.IsTrue(trie.HasWord(s)));
+            Assert.AreEqual(5, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixReturnsFalseIfSpecifiedPrefixIsDefaultChar()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemovePrefix(new string(new[] { default(char) })));
+        }
+
+        [Test]
+        public void TrieRemoveByPrefixDoesNotRemoveAnyNodesIfSpecifiedPrefixIsDefaultChar()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.RemovePrefix(new string(new[] { default(char) }));
+
+            words.ForEach(s =>
+                Assert.IsTrue(trie.HasWord(s)));
+            Assert.AreEqual(5, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByWordReturnsFalseIfTrieDoesNotContainWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemoveWord("FOO"));
+        }
+
+        [Test]
+        public void TrieRemoveByWordReturnsFalseIfTrieDoesNotContainStringAsWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemoveWord("AR"));
+        }
+
+        [Test]
+        public void TrieRemoveByWordReturnsTrueIfTrieContainsWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsTrue(trie.RemoveWord("ARM"));
+        }
+
+        [Test]
+        public void TrieRemoveByWordRemovesExpectedString()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.RemoveWord("ARM");
+
+            Assert.IsFalse(trie.HasWord("ARM"));
+            Assert.AreEqual(4, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByWordOnlyRemoveSpecifiedWord()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.RemoveWord("ARM");
+
+            Assert.IsTrue(trie.HasWord("ARMED"));
+            Assert.AreEqual(4, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByWordThrowsIfSpecifiedWordIsNull()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.Throws<ArgumentNullException>(() => trie.RemoveWord(null));
+        }
+
+        [Test]
+        public void TrieRemoveByWordReturnsFalseIfSpecifiedWordIsEmpty()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemoveWord(string.Empty));
+        }
+
+        [Test]
+        public void TrieRemoveByWordDoesNotRemoveAnyNodesIfSpecifiedWordIsEmpty()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            words.ForEach(s =>
+                Assert.IsTrue(trie.HasWord(s)));
+            Assert.AreEqual(5, trie.Count);
+        }
+
+        [Test]
+        public void TrieRemoveByWordReturnsFalseIfSpecifiedWordIsDefaultChar()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            Assert.IsFalse(trie.RemoveWord(new string(new[] { default(char) })));
+        }
+
+        [Test]
+        public void TrieRemoveByWordDoesNotRemoveAnyNodesIfSpecifiedWordIsDefaultChar()
+        {
+            var words = new List<string> { "ARM", "ARMED", "ARC", "JAWS", "JAZZ" };
+
+            var trie = new Trie();
+
+            trie.AddWords(words);
+
+            trie.RemoveWord(new string(new[] { default(char) }));
+
+            words.ForEach(s =>
+                Assert.IsTrue(trie.HasWord(s)));
+            Assert.AreEqual(5, trie.Count);
+        }
+    }
+}
---
+++ Dunk.Tools.Foundation/Text/TrieNode.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dunk.Tools.Foundation.Text
+{
+    /// <summary>
+    /// A node for <see cref="ITrie"/>
+    /// </summary>
+    [System.Diagnostics.DebuggerDisplay("Character = {Character}")]
+    public sealed class TrieNode : IEquatable<TrieNode>
+    {
+        private readonly Dictionary<char, TrieNode> _children;
+
+        /// <summary>
+        /// Initialises a new instance of <see cref="TrieNode"/> with a 
+        /// specified character.
+        /// </summary>
+        /// <param name="c">The character.</param>
+        public TrieNode(char c)
+        {
+            Character = c;
+            _children = new Dictionary<char, TrieNode>();
+        }
+
+        /// <summary>
+        /// Initialises a new instance of <see cref="TrieNode"/> with a 
+        /// flag indicating whether it is the root and a specified character.
+        /// </summary>
+        /// <param name="isRoot">The flag</param>
+        /// <param name="c"></param>
+        private TrieNode(bool isRoot, char c)
+        {
+            IsRoot = isRoot;
+            Character = c;
+            _children = new Dictionary<char, TrieNode>();
+        }
+
+        /// <summary>
+        /// Gets or sets the character stored in this node.
+        /// </summary>
+        public char Character 
+        { 
+            get; 
+            private set; 
+        }
+
+        /// <summary>
+        /// Gets or sets whether this node is the root.
+        /// </summary>
+        internal bool IsRoot
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets or sets whether this node is a leaf.
+        /// </summary>
+        internal bool IsLeaf
+        {
+            get;
+            set;
+        }
+
+        internal void Clear()
+        {
+            _children.Clear();
+        }
+
+        internal IEnumerable<TrieNode> GetChildren()
+        {
+            return _children.Values;
+        }
+
+        internal TrieNode GetChild(char character)
+        {
+            TrieNode childNode;
+            return _children.TryGetValue(character, out childNode) ? childNode : null;
+        }
+
+        internal void SetChild(TrieNode childNode)
+        {
+            _children[childNode.Character] = childNode;
+        }
+
+        internal TrieNode GetTrieNode(string prefix)
+        {
+            TrieNode trieNode = this;
+            for(int i = 0; i < prefix.Length; i++)
+            {
+                trieNode = trieNode.GetChild(prefix[i]);
+                if (trieNode == null)
+                {
+                    break;
+                }
+            }
+            return trieNode;
+        }
+
+        internal void RemoveChildNode(char c)
+        {
+            _children.Remove(c);
+        }
+
+        #region IEquatable<TrieNode> Members
+        public bool Equals(TrieNode other)
+        {
+            return other != null &&
+                other.Character == Character;
+        }
+        #endregion IEquatable<TrieNode> Members
+
+        public static TrieNode DefaultRootTrieNode => new TrieNode(true, default(char));
+    }
+}
